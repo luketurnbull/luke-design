2eeb4599c764814c1edbec159d0c4d109fff2ce8
Luke David Turnbull <lukedturnbull@gmail.com>
2025-02-08 13:01:38 +1100

    Updated dashboard with more details

--- app/routes/_authed/dashboard.tsx
+++ app/routes/_authed/dashboard.tsx
@@ -13,13 +13,24 @@ export const Route = createFileRoute('/_authed/dashboard')({
 
 function RouteComponent() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <CreateTShirtModal>
-        <Button>
-          <Plus />
-          Create T-Shirt
-        </Button>
-      </CreateTShirtModal>
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="flex flex-col gap-2 mb-12">
+        <h2 className="text-6xl font-bold mb-6">Design your own t-shirts</h2>
+        <p className="text-muted-foreground">
+          Get started by creating a new t-shirt design. click{' '}
+          <CreateTShirtModal>
+            <a className="text-blue-500 hover:underline cursor-pointer">here</a>
+          </CreateTShirtModal>{' '}
+          to create a new design, or open up the sidebar to get started.
+        </p>
+        <p className="text-muted-foreground">
+          Your designs will be saved to your account and can be accessed from
+          anywhere.
+        </p>
+        <p className="text-muted-foreground">
+          Share links to your designs with your friends and family.
+        </p>
+      </div>
     </div>
   )
 }
